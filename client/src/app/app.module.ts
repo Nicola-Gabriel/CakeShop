3caa45cd52a8d196a2b53b54ce94b2ec18e5ca73
Nicola-Gabriel <gabriel.nicola.00@gmail.com>
2021-01-26 10:55:50 +0200

    commit

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,24 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavComponent } from './nav/nav.component';
-import { ShopComponent } from './shop/shop.component';
-import { HttpClientModule } from '@angular/common/http';
-import { ProductItemComponent } from './shop/product-item/product-item.component';
-import { NgxFontAwesomeModule } from 'ngx-font-awesome';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavComponent,
-    ShopComponent,
-    ProductItemComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    NgxFontAwesomeModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
